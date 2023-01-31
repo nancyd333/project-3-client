609088504bcefdf737c6fec0f817225c9feecd73
ericcnguyen04 <ericc.win@gmail.com>
2023-01-31 18:51:53 -0500

    image displayed and redirect in making

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,33 +1,42 @@
 import '../css/Welcome.css'
+import 'bulma/css/bulma.min.css'
 
 export default function Welcome() {
 	return (
 		<div>
-			hello from welcome
+			<div>
+				<img src='img/images.jpeg' class='img' />
+			</div>
 
 			<div class='cards'>
 				<div id="cardtext1">
 					<strong>Reliable</strong>
 					<div class='sub-text'>
-						perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+						World's #1 home planner and development <br />
+						Obtain inspiration with people all around the world from cultural decoration to technology and advancement!
 					</div>
 				</div>
 				<div id="cardtext2">
-					<strong>Reliable</strong>
+					<strong>Resourceful</strong>
 					<div class='sub-text'>
-						perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+						Create your dream home by ensuring all resources and essentials needed in your home! Develop all the improvements needed in your home and impress your friends and family.
 					</div>
 				</div>
 				<div id="cardtext3">
-					<strong>Reliable</strong>
+					<strong>Renewable</strong>
 					<div class='sub-text'>
-						perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+						Keep up with society! <br />
+						Make sure you get the latest modern technology and strive for longevity! Recreate your house and transform into a home.
 					</div>
 				</div>
 				<div id="cardtext4">
-					<strong>Reliable</strong>
+					<strong>Reformed</strong>
 					<div class='sub-text'>
-						perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+						Life starts from the home! <br />
+						Start the improvement here and make a change!
+						<br />
+						<br />
+						<button class='button margin'>Start now</button>
 					</div>
 				</div>
 			</div>
