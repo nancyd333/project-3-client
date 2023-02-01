458d473629e49a4394459362e3531e0ab81ab399
ericcnguyen04 <ericc.win@gmail.com>
2023-01-31 19:31:58 -0500

    merging the edit page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,46 +1,85 @@
 import '../css/Welcome.css'
 import 'bulma/css/bulma.min.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Welcome() {
+
+	let navigate = useNavigate();
+	const routeChange = () =>{ 
+	  let path = `/items`; 
+	  navigate(path);
+	}
+
 	return (
 		<div>
 			<div>
-				<img src='img/images.jpeg' class='img' />
+				<img src='img/images.jpeg' className='img' />
 			</div>
 
-			<div class='cards'>
+			<div className='cards'>
 				<div id="cardtext1">
 					<strong>Reliable</strong>
-					<div class='sub-text'>
+					<div className='sub-text'>
 						World's #1 home planner and development <br />
 						Obtain inspiration with people all around the world from cultural decoration to technology and advancement!
 					</div>
 				</div>
 				<div id="cardtext2">
 					<strong>Resourceful</strong>
-					<div class='sub-text'>
+					<div className='sub-text'>
 						Create your dream home by ensuring all resources and essentials needed in your home! Develop all the improvements needed in your home and impress your friends and family.
 					</div>
 				</div>
 				<div id="cardtext3">
 					<strong>Renewable</strong>
-					<div class='sub-text'>
+					<div className='sub-text'>
 						Keep up with society! <br />
 						Make sure you get the latest modern technology and strive for longevity! Recreate your house and transform into a home.
 					</div>
 				</div>
 				<div id="cardtext4">
 					<strong>Reformed</strong>
-					<div class='sub-text'>
+					<div className='sub-text'>
 						Life starts from the home! <br />
 						Start the improvement here and make a change!
 						<br />
 						<br />
-						<button class='button margin'>Start now</button>
+						<button 
+						className='button margin'
+						onClick={routeChange}
+						>
+							Start now</button>
 					</div>
 				</div>
 			</div>
 
+
+			{/* <div class='cards'>
+				<div id="articletext1">
+					<strong>Reliable</strong>
+					<div className='sub-text'>
+						
+					</div>
+				</div>
+				<div id="articletext2">
+					<strong>Reliable</strong>
+					<div className='sub-text'>
+						
+					</div>
+				</div>
+				<div id="articletext3">
+					<strong>Reliable</strong>
+					<div className='sub-text'>
+						
+					</div>
+				</div>
+				<div id="articletext4">
+					<strong>Reliable</strong>
+					<div className='sub-text'>
+						
+					</div>
+				</div>
+			</div> */}
 		</div>
 	)
 }
