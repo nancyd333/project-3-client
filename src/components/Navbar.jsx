103ffe9a2864f30d297a286f1eea04483968bc70
ninambash <ninambash@gmail.com>
2023-02-02 09:41:50 -0800

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,63 @@
-import './css/Navbar.css'
-import 'bulma/css/bulma.min.css'
+import React from 'react';
+import 'bulma/css/bulma.min.css';
 
 export default function Navbar({ currentUser, handleLogout }) {
-	 const loggedIn = (
-		<div>
-			<div id="navbarBasicExample" className="navbar-menu">
-				<div className="navbar-end">
-				<div className="navbar-item">
+	const loggedIn = (
+		<div className="navbar-item">
+			<div className="buttons">
+				<a className="button is-light" href='/newItem'>
+					<strong>Share Item</strong>
+				</a>
 
-					<div className="buttons">
-						<a className="button is-light" href='/newItem'>
-							<strong>Share Item</strong>
-						</a>
-
-						{/* <a className="button is-light" href='/profile'>
-							<strong>Profile</strong>
-						</a> */}
-
-						<a className="button is-light" href='/' onClick={handleLogout}>
-							<strong>Logout</strong>
-						</a>
-					</div>
-
-				</div>
-				</div>
+				<a className="button is-light" href='/' onClick={handleLogout}>
+					<strong>Logout</strong>
+				</a>
 			</div>
 		</div>
-	 )
+	);
 
 	const loggedOut = (
-		<div>
-			<div id="navbarBasicExample" className="navbar-menu">
-				<div className="navbar-end">
-				<div className="navbar-item">
-					<div className="buttons">
-					<a className="button is-light" href='/register'>
-						<strong>Sign up</strong>
-					</a>
-					<a className="button is-light" href='/login'>
-						Log in
-					</a>
-					</div>
-				</div>
-				</div>
+		<div className="navbar-item">
+			<div className="buttons">
+				<a className="button is-light" href='/register'>
+					<strong>Sign up</strong>
+				</a>
+				<a className="button is-light" href='/login'>
+					Log in
+				</a>
 			</div>
-			
 		</div>
-	 )
+	);
 
 	return (
-		<div>
-			<nav className="color" role="navigation" aria-label="main navigation">
-				<div id="navbarBasicExample" className="navbar-menu">
-					<div className="navbar-start">
+		<nav className="navbar is-light" role="navigation" aria-label="main navigation">
+			<div className="container">
+				<div className="navbar-brand">
 					<a className="navbar-item" href='/'>
 						<strong>MERN Mavericks</strong>
 					</a>
 
-					<a className="navbar-item" href='/items'>
-						<strong>Items</strong>
+					<a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="true" data-target="navbarBasicExample">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
 					</a>
+				</div>
+
+				<div id="navbarBasicExample" className="navbar-menu">
+					<div className="navbar-start">
+						<a className="navbar-item" href='/items'>
+							<strong>Items</strong>
+						</a>
 					</div>
 
-					{currentUser ? loggedIn : loggedOut}
-
+					<div className="navbar-end">
+						{currentUser ? loggedIn : loggedOut}
+					</div>
 				</div>
-			</nav>
-		</div>
-	)
+			</div>
+		</nav>
+	);
 }
+
+
